c0b0a07e717f4a30906006b7fa0075a8696f5660
Brian Stearns <BrianW.Stearns@gmail.com>
2013-11-20 02:10:43 -0500

    Seeing how far I get on 99 cats before class.... annnnnnd go.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NineNineCats::Application.config.secret_token = '49d3bf3d8aa160cb2a4723ce62412318dcacd7bc5e7bc0d5ddf97b76516e74a2ca96dac53b91f31e9c89f1f8cdfbdd8a5d6aba209c7501aaeb37e14777b0d4da'
